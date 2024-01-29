00b2026eb7abcb43ecfe7a7df6aa2c5f4d23c5c8
richdede <deeprince2002@gmail.com>
2024-01-29 10:46:03 -0800

    deployment

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -22,7 +22,7 @@ const Footer = () => {
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Contact Us</h4>
           <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
-          <p>(333)425-6825</p>
+          <p>(090)6440-1844</p>
         </div>
       </div>
     </footer>
